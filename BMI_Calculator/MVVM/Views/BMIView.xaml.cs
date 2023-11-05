a37c950b1e234be3eef82d2aaf618c919c3ef9ca
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-05 18:10:55 +0200

    Link View with ViewModel

--- BMI_Calculator/MVVM/Views/BMIView.xaml.cs
+++ BMI_Calculator/MVVM/Views/BMIView.xaml.cs
@@ -1,3 +1,5 @@
+using BMI_Calculator.MVVM.ViewModels;
+
 namespace BMI_Calculator.MVVM.Views;
 
 public partial class BMIView : ContentPage
@@ -5,5 +7,6 @@ public partial class BMIView : ContentPage
 	public BMIView()
 	{
 		InitializeComponent();
+		BindingContext = new BMIViewModel();
 	}
 }
