952151714ad0481918c3412638ffcfd9c1ec5e55
Леня Шубин <lenyashubin2002@gmail.com>
2023-11-05 18:09:31 +0200

    Add BMIView view model class

---
+++ BMI_Calculator/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BMI_Calculator.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new BMI();
+            BMI.Height = 180;
+            BMI.Weight = 75;
+        }
+    }
+}
